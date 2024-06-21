0de0f2d1feab5bb98c7fc69c4f2c5d2e862a0b74
satryo220293 <satryo220293@gmail.com>
2024-06-21 09:59:44 +0000

    first commit

--- template/src/main.rs
+++ template/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{App, HttpServer, Responder};
+
+#[actix_web::get("/greet")]
+async fn greet() -> impl Responder {
+    format!("Hello World")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()>{
+    let port = 8080;
+    println!("Starting server on port {port}");
+
+    HttpServer::new(|| App::new().service(greet))
+        .bind(("127.0.0.1", port))?
+        .workers(2)
+        .run()
+        .await
+
+
 }
